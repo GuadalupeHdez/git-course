8c3e7090d54d9962fbb6d7947ca0e59b65f5ded7
MAGASCAF <MAGASCAF>
2016-07-09 10:39:50 -0500

    Se incorpora la utileria para enviar correo

--- src/com/qacg/Main.java
+++ src/com/qacg/Main.java
@@ -1,10 +1,12 @@
 package com.qacg;
 
+import com.qacg.utils.MailUtils;
+
 public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Hola mundo");
-
+		MailUtils.send("mghh_10@hotmail.com");
 	}
 
 }
---
+++ src/com/qacg/utils/MailUtils.java
@@ -0,0 +1,12 @@
+package com.qacg.utils;
+
+public class MailUtils {
+	public static boolean send(String email) {
+		if (email != null) {
+			System.out.println("Correo enviado a: "+email);
+			return true;	
+		} else 
+			return false;			
+	}
+
+}
