0c5084037acc7b1ed78eb201f85870daabb1961d
MAGASCAF <MAGASCAF>
2016-07-09 10:26:41 -0500

    Se incorpora la clase StringUtils

--- src/com/qacg/Main.java
+++ src/com/qacg/Main.java
@@ -1,9 +1,14 @@
 package com.qacg;
 
+import com.utils.StringUtils;
+
 public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Hola mundo");
+		String cadena = "Hola mundo";
+		
+		System.out.println(StringUtils.toLowerCase(cadena));
 
 	}
 
