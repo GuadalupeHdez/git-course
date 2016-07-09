8692479ebc380fac4f68989f11df0c9849e432a8
MAGASCAF <MAGASCAF>
2016-07-09 12:32:12 -0500

    Se agrego una nueva clase

---
+++ src/com/qacg/utils/ImprimirMensaje.java
@@ -0,0 +1,8 @@
+package com.qacg.utils;
+
+public class ImprimirMensaje {
+	public static String imprimirMensaje (String mensaje) {
+		System.out.println("El mensaje que viene como argumento es: "+mensaje);
+		return mensaje;
+	}
+}
